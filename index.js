6a568bde48e9d7de5d24fc402c546148df7bea8b
Brian Birdwell <bbirdwell@gmail.com>
2018-07-01 04:36:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
 }
+
+function deleteFromObjectsByKey(object, key) {
+  var newobj = Object.assign( {} )
+  delete newobject.key
+  return newobject
+  }
