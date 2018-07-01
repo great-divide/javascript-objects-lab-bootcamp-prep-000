c5589a312111b66935e7d09702d38df12615348a
Brian Birdwell <bbirdwell@gmail.com>
2018-07-01 04:32:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ var recipes = {}
 
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var newobj = object[key] = value
-  return newobj
+  return Object.assign( {}, obj, { [key]: value} )
 }
 
-return Object.assign( {}, obj, { [key]: value} )
